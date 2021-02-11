1b522552ac9348c9d1c86034ace5000ba497a0ec
alvinhuang <huang.alvin.01@gmail.com>
2021-02-11 13:57:29 -0800

    phase 1 almost done, check domcontent and button

--- connect-four.js
+++ connect-four.js
@@ -1,27 +1,43 @@
 import { Game } from "./game.js";
 let game = undefined;
-window.addEventListener("DCOMContentLoaded", (event) => {
-  let p1Input = document.getElementById("player-1-name");
-  let newGame = document.getElementById("new-game");
-  let p2Input = document.getElementById("player-2-name");
-  console.log('dom content')
-  let playerName = function () {
-    if (p1Input.value !== "" && p2Input.value !== "") {
-      newGame.setAttribute("disabled", "false");
-    } else {
-      newGame.setAttribute("disabled", "true")
-    }
+let boardHolder = document.getElementById("board-holder");
+let gameName = document.getElementById("game-name");
+let updateUI = function () {
+  if (game === undefined) {
+    boardHolder.classList.add("is-invisible");
+  } else {
+    boardHolder.classList.remove("is-invisible");
+    gameName.innerHTML = getName();
   }
+};
+// window.addEventListener("DCOMContentLoaded", (event) => {
+let p1Input = document.getElementById("player-1-name");
+let newGameBtn = document.getElementById("new-game");
+let p2Input = document.getElementById("player-2-name");
+console.log("dom content");
+let playerName = function () {
+  if (p1Input.value !== "" && p2Input.value !== "") {
+    newGameBtn.setAttribute("disabled", false);
+  } else {
+    newGameBtn.setAttribute("disabled", true);
+  }
+};
 
-  p1Input.addEventListener("keyup", (event) => {
-    playerName()
-    console.log('p1')
-  });
-
-  p2Input.addEventListener("keyup", (event) => {
-    playerName()
-    console.log('p2')
-  });
-
-
+p1Input.addEventListener("keyup", (event) => {
+  playerName();
+  console.log("p1");
 });
+
+p2Input.addEventListener("keyup", (event) => {
+  playerName();
+  console.log("p2");
+});
+
+newGameBtn.addEventListener("click", (event) => {
+  game = new Game(p1Input.value, p2Input.value);
+  p1Input.value = "";
+  p2Input.value = "";
+  newGameBtn.setAttribute("disabled", true);
+  updateUI();
+});
+// });
