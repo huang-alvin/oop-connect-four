f3d8f186f777b2f4ca79c7398e299636e16151e0
earlwoo <earl.woo@gmail.com>
2021-02-11 16:31:15 -0500

    2nd commit

--- connect-four.js
+++ connect-four.js
@@ -4,9 +4,24 @@ window.addEventListener("DCOMContentLoaded", (event) => {
   let p1Input = document.getElementById("player-1-name");
   let newGame = document.getElementById("new-game");
   let p2Input = document.getElementById("player-2-name");
-  p1Input.addEventListener("keyup", (event) => {
-    if (p1Input !== "" && p2Input !== "") {
-      newGame.setAttribute("disable", "false");
+  console.log('dom content')
+  let playerName = function () {
+    if (p1Input.value !== "" && p2Input.value !== "") {
+      newGame.setAttribute("disabled", "false");
+    } else {
+      newGame.setAttribute("disabled", "true")
     }
+  }
+
+  p1Input.addEventListener("keyup", (event) => {
+    playerName()
+    console.log('p1')
   });
+
+  p2Input.addEventListener("keyup", (event) => {
+    playerName()
+    console.log('p2')
+  });
+
+
 });
