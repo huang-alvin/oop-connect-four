dd65be719823c57189cdf2e2f5a8d0afb79e51f2
earlwoo <earl.woo@gmail.com>
2021-02-12 13:38:17 -0500

    going into add click handler

--- column.js
+++ column.js
@@ -1,11 +1,21 @@
 export default class Column{
     constructor(){
-
+        this.arr = [null, null, null, null, null, null]
     }
-    add(num){
-        // add num to bottom-most entry of column
+    add(playerNum){
+        for (let i = 0; i < this.arr.length; i++) {
+            if (this.arr[i] === null) {
+                this.arr[i] = playerNum
+            }
+        }
     }
-    getTokenAt(rowIdx, colIdx){
-        if()// if rowIdx === p2 return 2 ,else return null
+    getTokenAt(num){
+        if(this.arr[num] === null) {
+        return null;
+    } else if (this.arr[num] === 1) {
+        return 1;
+    } else if (this.arr[num] === 2) {
+        return 2;
     }
 }
+// if rowIdx === p2 return 2 ,else return null
--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 import Column from ('./column.js');
+
+
 export class Game {
   constructor(player1Name, player2Name) {
     this.player1 = player1Name;
@@ -13,11 +15,22 @@ export class Game {
     return `${this.player1} vs. ${this.player2}`;
   }
 
-  playInColumn() {
+  playInColumn(columnNum) {
+
+    this.columns[columnNum].add(this.currentPlayer)
+
     if (this.currentPlayer === 1) {
       this.currentPlayer = 2;
     } else {
       this.currentPlayer = 1;
     }
+  }
+
+  getTokenAt (rowIdx, colIdx) {
+    this.column = this.columns[colIdx]
+
+    return this.column.getTokenAt(rowIdx)
+    // colums[colIdx]
+
   }
 }
